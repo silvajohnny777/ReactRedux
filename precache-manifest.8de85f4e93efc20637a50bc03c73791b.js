cd1df99e08adeb7bf7123fa106d34137fd33b12b
silvajohnny777 <jonatha_silva2012@hotmail.com>
2019-08-19 18:27:47 -0300

    Updates

---
+++ precache-manifest.8de85f4e93efc20637a50bc03c73791b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f97629f2dfbb8a30ac0dd0456b552a6",
+    "url": "/ReactRedux/index.html"
+  },
+  {
+    "revision": "b0e4a5b74b3523ab092a",
+    "url": "/ReactRedux/static/js/2.625d14f8.chunk.js"
+  },
+  {
+    "revision": "3099a0ce77c7d7502e63",
+    "url": "/ReactRedux/static/js/main.a81467a0.chunk.js"
+  },
+  {
+    "revision": "02a496e8174e3c9c10cd",
+    "url": "/ReactRedux/static/js/runtime~main.c857f4ad.js"
+  }
+]);
